7c33d01bddc2a7fb0e144a6c65d0efaca9f356e6
tekihei2317 <tekihei2317@gmail.com>
2025-04-22 23:28:57 +0900

    モンスターを表示する

--- classic-rpg/src/App.tsx
+++ classic-rpg/src/App.tsx
@@ -1,18 +1,44 @@
 import { useState } from "react";
 import {
+  CHARACTER_MONSTER,
   CHARACTER_PLAYER,
   CharacterStatus,
   MONSTER_PLAYER,
+  MONSTER_SLIME,
   monsters,
 } from "./core";
 import "./App.css";
+
+function waitForEnter() {
+  return new Promise<void>((resolve) => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Enter") {
+        window.removeEventListener("keydown", handler);
+        resolve(); // ユーザーがEnterを押したら解決
+      }
+    };
+    window.addEventListener("keydown", handler);
+  });
+}
 
 function App() {
   // プレイヤーのステータスを初期化する
   // ↑これstateで持つのかrefで持つのかどっちがいいんだろうね...
   const [characters, setCharacters] = useState<CharacterStatus[]>([
     monsters[MONSTER_PLAYER],
+    monsters[MONSTER_SLIME],
   ]);
+  console.log(characters[CHARACTER_MONSTER].aa);
+
+  const [messages, setMessages] = useState<string[]>([]);
+
+  const handleAttack = async () => {
+    setMessages(["ゆうしゃ　のこうげき！"]);
+
+    // キー入力を待つ
+    await waitForEnter();
+    setMessages(["スライム　に3のダメージ！"]);
+  };
 
   return (
     <>
@@ -24,6 +50,19 @@ function App() {
             {characters[CHARACTER_PLAYER].maxHp} MP：
             {characters[CHARACTER_PLAYER].mp}／
             {characters[CHARACTER_PLAYER].maxHp}
+          </div>
+
+          <div>
+            <div style={{ whiteSpace: "pre-wrap", marginTop: "1rem" }}>
+              {characters[CHARACTER_MONSTER].aa}
+              （H P：{characters[CHARACTER_MONSTER].hp}／
+              {characters[CHARACTER_MONSTER].maxHp}）
+            </div>
+          </div>
+          <div>
+            {messages.map((message, index) => (
+              <div key={index}>{message}</div>
+            ))}
           </div>
         </div>
       </div>
--- classic-rpg/src/core.ts
+++ classic-rpg/src/core.ts
@@ -7,10 +7,13 @@ export type CharacterStatus = {
   mp: number;
   maxMp: number;
   name: string;
+  /** アスキーアート */
+  aa: string;
 };
 
 export const MONSTER_PLAYER = 0;
-export const MONSTER_MAX = 1;
+export const MONSTER_SLIME = 1;
+export const MONSTER_MAX = 2;
 
 export const CHARACTER_PLAYER = 0;
 export const CHARACTER_MONSTER = 1;
@@ -26,5 +29,15 @@ export const monsters: CharacterStatus[] = [
     mp: 15,
     maxMp: 15,
     name: "ゆうしゃ",
+    aa: "",
+  },
+  {
+    hp: 3,
+    maxHp: 3,
+    mp: 0,
+    maxMp: 3,
+    name: "スライム",
+    aa: `／・Д・＼
+～～～～～`,
   },
 ];
