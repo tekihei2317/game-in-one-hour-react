b8cdbb0864253c0e517e8b7b6b37019220cb8060
tekihei2317 <tekihei2317@gmail.com>
2025-04-27 17:10:18 +0900

    次の世代への更新処理を実装

--- lifegame/src/App.tsx
+++ lifegame/src/App.tsx
@@ -4,7 +4,7 @@ const FIELD_WIDTH = 12;
 const FIELD_HEIGHT = 12;
 
 type Field = boolean[][];
-const EMPTY_ROW = new Array(FIELD_WIDTH).fill(0);
+const EMPTY_ROW = new Array(FIELD_WIDTH).fill(false);
 
 /**
  * フィールドを表示する
@@ -12,8 +12,8 @@ const EMPTY_ROW = new Array(FIELD_WIDTH).fill(0);
 function Field({ field }: { field: Field }) {
   return (
     <div>
-      {field.map((row) => (
-        <div>{row.map((cell) => (cell ? "■" : "　")).join("")}</div>
+      {field.map((row, index) => (
+        <div key={index}>{row.map((cell) => (cell ? "■" : "　")).join("")}</div>
       ))}
     </div>
   );
@@ -25,14 +25,11 @@ function Field({ field }: { field: Field }) {
 function countAliveNeighbors(field: Field, row: number, column: number) {
   let count = 0;
 
-  // TODO: フィールドの範囲外に出た時に、ループさせる
   for (let dy = -1; dy <= 1; dy++) {
-    const y = row + dy;
-    if (y < 0 || y >= FIELD_HEIGHT) continue;
+    const y = (row + dy + FIELD_HEIGHT) % FIELD_HEIGHT;
 
     for (let dx = -1; dx <= 1; dx++) {
-      const x = column + dx;
-      if (x < 0 || x >= FIELD_WIDTH) continue;
+      const x = (column + dx + FIELD_WIDTH) % FIELD_WIDTH;
 
       // 自身は除外する
       if (y === row && x === column) continue;
@@ -40,21 +37,58 @@ function countAliveNeighbors(field: Field, row: number, column: number) {
       count += field[y][x] ? 1 : 0;
     }
   }
+
   return count;
+}
+
+/**
+ * 次の世代を返す
+ */
+function nextGeneration(field: Field): Field {
+  const newField = [...new Array(FIELD_HEIGHT)].map(() =>
+    new Array(FIELD_WIDTH).fill(false)
+  );
+
+  for (let i = 0; i < FIELD_HEIGHT; i++) {
+    for (let j = 0; j < FIELD_WIDTH; j++) {
+      const count = countAliveNeighbors(field, i, j);
+      newField[i][j] = field[i][j];
+
+      if (field[i][j]) {
+        // 過疎による死滅
+        if (count < 2) newField[i][j] = false;
+
+        // 過密による死滅
+        if (count >= 4) newField[i][j] = false;
+      } else {
+        // 誕生
+        if (count === 3) newField[i][j] = true;
+      }
+    }
+  }
+
+  return newField;
 }
 
 function App() {
   const [field, setField] = useState<Field>([
-    [0, 1, 0, ...new Array(FIELD_WIDTH - 3).fill(0)],
-    [0, 0, 1, ...new Array(FIELD_WIDTH - 3).fill(0)],
-    [1, 1, 1, ...new Array(FIELD_WIDTH - 3).fill(0)],
-    ...new Array(FIELD_HEIGHT - 3).fill(EMPTY_ROW),
+    [false, true, false, ...new Array(FIELD_WIDTH - 3).fill(false)],
+    [false, false, true, ...new Array(FIELD_WIDTH - 3).fill(false)],
+    [true, true, true, ...new Array(FIELD_WIDTH - 3).fill(false)],
+    ...[...new Array(FIELD_HEIGHT - 3)].map(() => EMPTY_ROW),
   ]);
 
+  // Enterを押すごとにシミュレーションを進める
   useEffect(() => {
-    for (let i = 0; i < FIELD_HEIGHT; i++) {
-      console.log(field[i]);
-    }
+    const handle = (e: KeyboardEvent) => {
+      if (e.key === "Enter") {
+        setField(nextGeneration(field));
+      }
+    };
+
+    window.addEventListener("keydown", handle);
+
+    return () => window.removeEventListener("keydown", handle);
   }, [field]);
 
   return (
